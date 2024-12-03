ab350b947a5965cb47532dd25cc58150b70773d6
bryanteng <bteng1226@gmail.com>
2024-12-02 19:36:19 -0500

    game logic for points change, more style changes, more responsive, middle  start button

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -3,9 +3,13 @@ import './Grid.css';
 import {generateRandomHex, calculateTintAndShade} from './util'
 
 const Grid = () => {
-  const [difficulty, setDifficulty] = useState(1);
+  const [difficulty, setDifficulty] = useState(0);
+  const [level, setLevel] = useState(1);
+
+  const [score, setScore] = useState(0)
+  const [tries, setTries] = useState(0)
+
   const [colors, setColors] = useState(Array.from({ length: 16 }).fill('#000000'));
-  const [highlightColor, setHighlightColor] = useState('');
   const [highlightIndex, setHighlightIndex] = useState(null);
 
   const [time, setTime] = useState(0); // Time in milliseconds
@@ -25,21 +29,26 @@ const Grid = () => {
     }
 
     return () => clearInterval(timerRef.current); // Cleanup on unmount
-  }, [isRunning]);
+  }, [isRunning, difficulty]);
 
   const resetTimer = () => {
     clearInterval(timerRef.current);
     setTime(0);
-    const startTime = Date.now();
-    timerRef.current = setInterval(() => {
-      setTime(Date.now() - startTime);
-    }, 10);
+    setIsRunning(false)
   };
+
+  const startGame = () => {
+    resetTimer()
+    setScore(0)
+    setLevel(1)
+    setDifficulty(0)
+    setIsRunning(true)
+  }
 
   useEffect(() => {
     if(isRunning){
       const color = generateRandomHex()
-      const {tint, shade} = calculateTintAndShade(color, (40-difficulty)/100)
+      const {tint, shade} = calculateTintAndShade(color, (30-difficulty)/100)
       const shades = Array.from({ length: 16 }).fill(color);
   
       const randomIndex = Math.floor(Math.random() * 16);
@@ -58,7 +67,18 @@ const Grid = () => {
     const isCorrect = parseInt(e.target.id) === highlightIndex;
     console.log(isCorrect ? 'Correct!' : 'Try again!');
     if (isCorrect) {
-      setDifficulty(difficulty+1)
+      setDifficulty(difficulty+2)
+      // scoring calculation criteria
+      // fast -> track how much time it takes to get the right answer 
+      // difficult -> more difficult more points
+      // tries -> wrong answer increment tries and deduct from score, could deduct points immediately?
+      setScore(score + (100 * (1 - tries/10) ))
+      setTries(0)
+      setLevel(level + 1)
+     // check for game end round (15) and end the game, maybe show leaderboard?
+    } else {
+      setTries(tries+1)
+      // setScore(score - 10)
     }
   };
 
@@ -68,16 +88,31 @@ const Grid = () => {
     return `${seconds}.${milliseconds.toString().padStart(2, '0')}s`;
   };
 
+  // score calculation table by round 
+  // round | tries  | time taken | score
+  // 1     | 1      | 0.00s      | 100
+  // 2     | 1      | 0.00s      | 200
+  // ------------------------------------
+  //                             | 300
+
+  // Leaderboard -> only persist 3 to 10 scores in localstorage.
+  // name  | score
+  // a     | 100
+  // b     | 200
+
   return (
     <div className="page-container">
       <div> 
-        <h1>Difficulty: {difficulty}</h1>
+        <h1>Level: {level} Difficulty: {difficulty}</h1>
+        <h1>score: {score}</h1>
+
         <div className="timer">
           Time: {formatTime(time)}
         </div>
         <button id="down" onClick={()=> console.log(colors)}> colros</button>
 
-        <button id="starttimer" onClick={()=> setIsRunning(!isRunning)}> start </button>
+        <button id="resettimer" onClick={resetTimer}> reset </button>
+
         <button id="down" onClick={changeDifficulty}>
           -
         </button>
@@ -92,7 +127,7 @@ const Grid = () => {
             id={index}
             key={index}
             onClick={spanClick}
-            className="grid-item"
+            className={`grid-item ${time > 1 ? 'spin-animation' : null}`}
             style={{
               backgroundColor: color
             }}
@@ -100,6 +135,7 @@ const Grid = () => {
             {"       "}
           </span>
         ))}
+        <button id="starttimer" className="startButton" onClick={startGame}> start </button>
       </div>
     </div>
   );
